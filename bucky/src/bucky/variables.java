3065bf667966ef029c80dea0a96f147695b33584
Cameron <cameron.p.buckingham@gmail.com>
2015-06-26 11:11:52 -0500

    variables

---
+++ bucky/src/bucky/variables.java
@@ -0,0 +1,11 @@
+package bucky;
+
+class variables {
+    public static void main(String args[]){
+        double tuna;
+        tuna = 5.28;
+        System.out.println ("I want ")
+        System.out.print("tuna");
+        System.out.println (" movies")
+      }
+}
