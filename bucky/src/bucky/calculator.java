637ddf8b4bd0d1231ae569bf0c5b25ca01fb1b37
Cameron <cameron.p.buckingham@gmail.com>
2015-06-26 11:33:03 -0500

    still working

---
+++ bucky/src/bucky/calculator.java
@@ -0,0 +1,19 @@
+
+package bucky;
+
+import java.util.Scanner;
+
+
+public class calculator {
+	public static void main(String arg[]){
+		Scanner bucky = new Scanner(System.in);
+		double fnum, snum, answer;
+		System.out.println("Enter First Num: ");
+		fnum = bucky.nextDouble();
+		System.out.println("Enter second num: ");
+		snum = bucky.nextDouble();
+		answer = fnum + snum;
+		System.out.println(answer);
+		
+	}
+}
--- bucky/src/bucky/variables.java
+++ bucky/src/bucky/variables.java
@@ -4,8 +4,8 @@ class variables {
     public static void main(String args[]){
         double tuna;
         tuna = 5.28;
-        System.out.println ("I want ")
+        System.out.println ("I want ");
         System.out.print(tuna);
-        System.out.println (" movies")
+        System.out.println (" movies");
       }
 }
