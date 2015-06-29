14e07402b8b821b2e422012aacf15ee85571b3c0
Cameron <cameron.p.buckingham@gmail.com>
2015-06-28 20:53:27 -0700

    scanner

--- bucky/src/bucky/calculator.java
+++ bucky/src/bucky/calculator.java
@@ -13,7 +13,7 @@ public class calculator {
 		System.out.println("Enter second num: ");
 		snum = bucky.nextDouble();
 		answer = fnum + snum;
-		System.out.println(answer);
+		System.out.println();
 		
 	}
 }
---
+++ bucky/src/bucky/scanner.java
@@ -0,0 +1,10 @@
+
+package bucky;
+import java.util.Scanner;
+
+class scanner {
+	public static void main(String args[]){
+	Scanner cam = new Scanner(System.in);
+	System.out.println(cam.nextLine());
+	}
+}
