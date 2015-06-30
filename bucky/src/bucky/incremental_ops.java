4df365fad28060c69daabaac1a167ea7596d422b
Cameron <cameron.p.buckingham@gmail.com>
2015-06-30 08:52:28 -0700

    incremental ops

---
+++ bucky/src/bucky/incremental_ops.java
@@ -0,0 +1,15 @@
+package bucky;
+
+public class incremental_ops {
+	public static void main(String args[]){
+		
+		int tuna = 5;
+		int bass = 18;
+		++tuna;
+		tuna +=5;
+		//System.out.println(tuna);
+		System.out.println(tuna--);
+		System.out.println(tuna);
+
+	}
+}
