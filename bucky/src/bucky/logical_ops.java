e10c5fd8f5d016fc627d7c3809c83e162c0efc42
Cameron <cameron.p.buckingham@gmail.com>
2015-06-30 09:59:19 -0700

    logical operators

---
+++ bucky/src/bucky/logical_ops.java
@@ -0,0 +1,18 @@
+package bucky;
+
+public class logical_ops {
+	public static void main (String args[]){
+		int boy, girl;
+		boy=18;
+		girl=64;
+		
+		if(boy>10 && girl > 60){
+	    //if(boy>10 || girl > 60){
+			System.out.println("you can enter");
+			
+		}
+		else{
+				System.out.println("You are too young");
+	}
+}
+	}
