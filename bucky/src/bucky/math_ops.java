5532f0f9707f5a66723f815c749d9ff33b7a5320
Cameron <cameron.p.buckingham@gmail.com>
2015-06-29 20:03:07 -0700

    math operators

---
+++ bucky/src/bucky/math_ops.java
@@ -0,0 +1,22 @@
+package bucky;
+
+public class math_ops {
+	public static void main(String args[]){
+
+	int girls, boys, add, sub, div, mul, mod;
+	girls = 11;
+	boys = 3;
+	add =  girls + boys;
+	sub = girls - boys;
+	div = girls / boys;
+	mul = girls * boys;
+	mod = girls % boys;
+	System.out.println("Numbers are "+ boys + " and "+ girls);
+	System.out.println(add);
+	System.out.println(sub);
+	System.out.println(div);
+	System.out.println(mul);
+	System.out.println(mod);		
+	}
+
+}
