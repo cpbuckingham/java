16b847c742332c4eb28ecaaa711d1d6f2b26ada2
Cameron <cameron.p.buckingham@gmail.com>
2015-06-30 09:42:23 -0700

    if_statements

---
+++ bucky/src/bucky/if_statement.java
@@ -0,0 +1,14 @@
+package bucky;
+
+public class if_statement {
+	public static void main(String arg[]){
+		int test = 9;
+				
+		if(test == 9){
+		//if(test < 9){
+			System.out.println("yes");
+		}else{
+			System.out.println("no");
+		}
+	}
+}
