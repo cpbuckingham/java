35a06e36ad67293c885bf2f352f500e1846b61ec
Cameron <cameron.p.buckingham@gmail.com>
2015-07-01 12:36:51 -0700

    while loops

---
+++ bucky/src/bucky/while_loop.java
@@ -0,0 +1,11 @@
+package bucky;
+
+public class while_loop {
+	public static void main (String args[]){
+		int counter = 0;
+		while(counter < 1000000000){
+			System.out.println(counter);
+			counter++;
+		}
+	}
+}
