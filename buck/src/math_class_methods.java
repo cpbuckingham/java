b496e642ce72485971ea9cfad40aba08971a0a3b
Cameron <cameron.p.buckingham@gmail.com>
2015-07-03 10:04:53 -0700

    math methods

---
+++ buck/src/math_class_methods.java
@@ -0,0 +1,14 @@
+
+public class math_class_methods {
+
+	public static void main(String[] args) {
+		System.out.println(Math.abs(-26.8));
+		System.out.println(Math.ceil(7.6));
+		System.out.println(Math.floor(7.6));
+		System.out.println(Math.max(8.6, 5.2));
+		System.out.println(Math.min(8.6, 5.2));
+		System.out.println(Math.pow(5,4));
+		System.out.println(Math.sqrt(9));
+	}
+
+}
