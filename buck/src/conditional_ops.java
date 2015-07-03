04f707f752c34a3038c4bc798f6dede7ea2dd226
Cameron <cameron.p.buckingham@gmail.com>
2015-07-03 09:51:26 -0700

    compounded interest

---
+++ buck/src/conditional_ops.java
@@ -0,0 +1,10 @@
+
+
+public class conditional_ops {
+	public static void main(String args[]){
+		int age= 61;
+		
+		System.out.println(age>50 ? "You are old" : "You are young");
+		
+	}
+}
---
+++ buck/src/for_loops.java
@@ -0,0 +1,8 @@
+
+public class for_loops {
+	public static void main (String[] args){
+		for(int counter=0;counter<=10;counter++){
+			System.out.println(counter);
+		}
+	}
+}
