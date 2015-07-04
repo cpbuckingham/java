b818afb12a19dae37b06a8cffaa84e5b415cc6be
Cameron <cameron.p.buckingham@gmail.com>
2015-07-04 10:16:57 -0700

    written out array

--- buck/src/arrays.java
+++ buck/src/arrays.java
@@ -1,12 +1,14 @@
 
 public class arrays {
 	public static void main(String[] args){
-		int bucky[]= new int [10];
 		
-		bucky[0]=87;
-		bucky[1]=543;
-		bucky[9]=65;
+		//int bucky[]= new int [10];
+		//bucky[0]=87;
+		//bucky[1]=543;
+		//bucky[9]=65;
 		
-		System.out.println(bucky[11]);
+		int bucky []={2,3,4,5,6,7,8,10,9};
+		
+		System.out.println(bucky[2]);
 	}
 }
