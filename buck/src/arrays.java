f06cb0f0444594421fe4dc284bbf6480ef19fc0d
Cameron <cameron.p.buckingham@gmail.com>
2015-07-04 10:14:55 -0700

    basic arrays

---
+++ buck/src/arrays.java
@@ -0,0 +1,12 @@
+
+public class arrays {
+	public static void main(String[] args){
+		int bucky[]= new int [10];
+		
+		bucky[0]=87;
+		bucky[1]=543;
+		bucky[9]=65;
+		
+		System.out.println(bucky[11]);
+	}
+}
