d7c12f887ca5b5beaf12f7de0559fce707152145
Cameron <cameron.p.buckingham@gmail.com>
2015-07-05 10:05:09 -0700

    multidimensional arrays

---
+++ buck/src/multidimensional_arr.java
@@ -0,0 +1,10 @@
+
+public class multidimensional_arr {
+	public static void main (String[] args){
+		int firstarray[][]={{8,9,10,11},{12,13,14,15}};
+		System.out.println(firstarray[1][1]);
+		int secondarray[][]={{30,31,32,33},{43},{4,5,6}};
+		System.out.println(secondarray[2][0]);
+
+	}
+}
