c0e76ed20c0df2d5098716f921991fb1e456ea37
Cameron <cameron.p.buckingham@gmail.com>
2015-07-07 07:11:04 -0700

    time class

---
+++ buck/src/time_class.java
@@ -0,0 +1,23 @@
+
+public class time_class {
+		private int hour;
+		private int minute;
+		private int second;
+		
+		
+		public void setTime(int h, int m, int s){
+			hour = ((h>0 && h<23) ? h : 0);
+			minute = ((m>0 && m<60) ? m : 0);
+			second = ((s>0 && h<60) ? s : 0);
+			
+		}
+		public String toMilitary(){
+			return String.format("%02d:%02d:%02d", hour, minute, second );
+		}
+		public static void main(String[]args){
+			time_class time_classObject = new time_class();
+			System.out.println(time_classObject.toMilitary());
+			time_classObject.setTime(14,45,73);
+			System.out.println(time_classObject.toMilitary());
+		}
+	}
