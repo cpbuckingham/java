c8a418495c36bc0663c39756bbe06c15ad523509
Cameron <cameron.p.buckingham@gmail.com>
2015-08-06 07:04:41 -0700

    clementines class

---
+++ buck/src/buttons/clementines.java
@@ -0,0 +1,14 @@
+package buttons;
+
+import javax.swing.JFrame;
+
+public class clementines {
+
+	public static void main(String[] args) {
+		jcheckbox_pt2 go = new jcheckbox_pt2();
+		go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		go. setSize(300,200);
+		go.setVisible(true);
+	}
+
+}
