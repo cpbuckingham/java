86ce2be67cb35ae43af73e15a9a46767c14c4b31
Cameron <cameron.p.buckingham@gmail.com>
2015-08-05 17:08:48 -0700

    apples class

---
+++ buck/src/buttons/apples.java
@@ -0,0 +1,12 @@
+package buttons;
+
+import javax.swing.JFrame;
+
+public class apples {
+	public static void main(String[] args) {
+		Gui_pt2 go = new Gui_pt2();
+		go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		go. setSize(300,200);
+		go.setVisible(true);
+	}
+}
