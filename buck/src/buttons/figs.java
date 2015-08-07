b953d252103d8a8f80cafdcc6a07fdddf2b44b1b
Cameron <cameron.p.buckingham@gmail.com>
2015-08-07 05:22:06 -0700

    figs class

---
+++ buck/src/buttons/figs.java
@@ -0,0 +1,14 @@
+package buttons;
+
+import javax.swing.JFrame;
+
+public class figs {
+
+	public static void main(String[] args) {
+		jradiobutton_pt2 go = new jradiobutton_pt2();
+		go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		go. setSize(300,200);
+		go.setVisible(true);
+	}
+
+}
