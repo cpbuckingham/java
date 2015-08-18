9de6467f810e26702cd9692b9a3d1c90c5993379
Cameron <cameron.p.buckingham@gmail.com>
2015-08-18 06:03:27 -0500

    grapes class

---
+++ buck/src/last/grapes.java
@@ -0,0 +1,12 @@
+package last;
+
+import javax.swing.JFrame;
+
+public class grapes {
+	public static void main(String[] args) {
+		combo_box go = new combo_box();
+		go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		go. setSize(300,200);
+		go.setVisible(true);
+	}
+}
