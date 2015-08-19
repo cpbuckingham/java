30689b3449a94c1fdcf6af40933a13a757c5449b
Cameron <cameron.p.buckingham@gmail.com>
2015-08-19 07:26:57 -0700

    kiwi class

---
+++ buck/src/last/kiwi.java
@@ -0,0 +1,12 @@
+package last;
+
+import javax.swing.JFrame;
+
+public class kiwi {
+	public static void main(String[] args) {
+		multiple_list go = new multiple_list();
+		go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		go. setSize(300,200);
+		go.setVisible(true);
+	}
+}
