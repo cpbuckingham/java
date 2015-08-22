f3ce66d8bfb58aff387fcba87d259a6eccda9e9e
Cameron <cameron.p.buckingham@gmail.com>
2015-08-22 08:57:50 -0700

    plums class:

---
+++ buck/src/last/plums.java
@@ -0,0 +1,12 @@
+package last;
+
+import javax.swing.JFrame;
+
+public class plums {
+	public static void main(String[] args) {
+		mouse_events go = new mouse_events();
+		go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		go. setSize(300,200);
+		go.setVisible(true);
+	}
+}
