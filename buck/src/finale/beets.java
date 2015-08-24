0566a3c35fcfb4f4647dccbdc4b0ce778a42de44
Cameron <cameron.p.buckingham@gmail.com>
2015-08-24 07:31:32 -0700

    beets class

---
+++ buck/src/finale/beets.java
@@ -0,0 +1,16 @@
+package finale;
+
+import javax.swing.*;
+
+public class beets {
+	public static void main (String[] args){
+	
+	JFrame go=new JFrame("choose color");
+	choose_color p = new choose_color();
+	go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+}
+}
+
+
+
