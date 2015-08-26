acc2cdcb648c3f053244ea7ff66c2b7216b3cddb
Cameron <cameron.p.buckingham@gmail.com>
2015-08-26 07:30:35 -0700

    carrot class

---
+++ buck/src/finale/carrots.java
@@ -0,0 +1,15 @@
+package finale;
+
+import javax.swing.*;
+
+public class carrots {
+	public static void main (String[] args){
+	flowlayout go = new flowlayout();
+	go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	go. setSize(300,200);
+	go.setVisible(true);
+}
+}
+
+
+
