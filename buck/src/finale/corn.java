f799d0220fcf8a9158471ab47dc1e1731c21f27a
Cameron <cameron.p.buckingham@gmail.com>
2015-08-28 08:41:11 -0700

    added corn class

---
+++ buck/src/finale/corn.java
@@ -0,0 +1,15 @@
+package finale;
+
+public class corn {
+
+	public static void main(String[] args) {
+
+			read_file g = new read_file();
+			g.openFile();
+			g.readFile();
+			g.closeFile();
+			
+			
+	}
+
+}
