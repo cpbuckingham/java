36cbcf9c2559065a45cd23e265f82d4c07cd59b5
Cameron <cameron.p.buckingham@gmail.com>
2015-09-02 07:31:59 -0700

    added error class

---
+++ buck/src/finale/error.java
@@ -0,0 +1,26 @@
+package finale;
+
+import java.util.*;
+
+public class error {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int x=1;
+		do{
+		try{
+		System.out.println("Enter first num");
+		int n1 = input.nextInt();
+		System.out.println("Enter second num");
+		int n2 = input.nextInt();
+		int sum = n1/n2;
+		System.out.println(sum);
+		x=2;
+		}
+		catch(Exception e){
+			System.out.println("error");
+		}
+			
+	}while(x==1);
+	}
+}
