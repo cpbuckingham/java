90f4a9a0ca98d758ab200ddbebb418f7d7638bd0
Cameron <cameron.p.buckingham@gmail.com>
2015-09-05 06:27:12 -1000

    added finale class

---
+++ buck/src/finale/finale.java
@@ -0,0 +1,18 @@
+package finale;
+
+public class finale {
+	public static void main(String [] args){
+
+	String a = "apples";
+	String b = "buck";
+	String c = "BUCK";
+	System.out.println(a.length());
+	
+	if(a.equals("apples")){
+		System.out.println("it equals apples!");
+	}
+	if(b.equalsIgnoreCase(c)){
+		System.out.println("buck match");
+	}	
+	}
+}
