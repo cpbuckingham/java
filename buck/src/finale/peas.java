b968d11758c8b470b87fc3d9c595fd6e82d599ca
Cameron <cameron.p.buckingham@gmail.com>
2015-09-10 07:25:31 -0700

    added pea class

---
+++ buck/src/finale/peas.java
@@ -0,0 +1,15 @@
+package finale;
+
+public class peas {
+
+	public static void main(String[] args) {
+
+			write_file g = new write_file();
+			g.openFile();
+			g.addRecords();
+			g.closeFile();
+			
+			
+	}
+
+}
