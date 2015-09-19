b8de466a403763e507495d6c87b73d869241a1ce
Cameron <cameron.p.buckingham@gmail.com>
2015-09-19 08:28:53 -0700

    recursion class added

---
+++ java_medium/src/java_medium/recursion.java
@@ -0,0 +1,15 @@
+package java_medium;
+
+public class recursion {
+
+	public static void main(String[] args) {
+		System.out.println(fact(5));
+	}
+
+	public static long fact(long n){
+		if (n<=1)
+			return 1;
+		else 
+			return n * fact(n-1);
+	}
+}
