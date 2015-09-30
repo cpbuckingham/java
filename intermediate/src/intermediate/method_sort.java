ffc332f3b0416181e241fabdf5f1bc84d9ba689f
Cameron <cameron.p.buckingham@gmail.com>
2015-09-30 08:01:14 -0700

    added sort method

---
+++ intermediate/src/intermediate/method_sort.java
@@ -0,0 +1,18 @@
+package intermediate;
+
+import java.util.*;
+
+public class method_sort {
+  public static void main(String[] args)
+{
+String[]stuff = {"apples", "lemons", "geese", "bacon", "youtube"};
+List<String> l1 = Arrays.asList(stuff);
+
+Collections.sort(l1);
+System.out.printf("%s\n", l1);
+
+Collections.sort(l1, Collections.reverseOrder());
+System.out.printf("%s\n", l1);
+
+}
+}
