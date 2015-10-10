38fe72db2699d1e9cb39c39c1c838cbde42ba81c
Cameron <cameron.p.buckingham@gmail.com>
2015-10-10 09:01:25 -0500

    added Browser test class

---
+++ intermediate/src/done_intermediate/BrowserTest.java
@@ -0,0 +1,10 @@
+package done_intermediate;
+
+import javax.swing.JFrame;
+
+public class BrowserTest{
+  public static void main(String[] args){
+    Browser stuff = new Browser();
+    stuff.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+}
